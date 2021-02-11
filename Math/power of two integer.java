e8db4b82bc594fb07fa550db84a0b361d1b50a1f
priyanshu jain <68162762+priyanshu003@users.noreply.github.com>
2021-02-11 11:14:23 +0530

    Create power of two integer.java

---
+++ Math/power of two integer.java
@@ -0,0 +1,24 @@
+/*
+Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
+
+Example
+
+Input : 4
+Output : True  
+as 2^2 = 4. */
+
+
+public class Solution {
+    public int isPower(int A) {
+        int a = (int)Math.sqrt(A);
+        if(a*a == A)return  1;
+        for(int i = a-1;i>1;i--){
+            for(int j = 2;j<Integer.MAX_VALUE;j++){
+                if(Math.pow(i,j) == A){
+                    return 1;
+                }if(Math.pow(i,j) > A)break;
+            }
+        }
+        return 0;
+    }
+}
